fd4b8c1f845af353470f614f3c92d8da2dd86975
Param-2006 <69540098+Param-2006@users.noreply.github.com>
2020-10-16 16:23:03 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+//Create variables here
+var dog,happyDog,database,foodS,foodStock;
+var database;
+
+function preload()
+{
+  //load images here
+  image1=loadImage("images/dogImg.png");
+  image2=loadImage("images/dogImg1.png");
+}
+
+function setup() {
+	createCanvas(500, 500);
+  dog=createSprite(300,300);
+  dog.addImage("dogImg",image1);
+  foodS=0;
+  database=firebase.database();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStocks);
+}
+
+
+function draw() {  
+
+  background(46,139,87);
+
+  if(keyWentDown(UP_ARROW)){
+    dog.addImage("dogImg",image2);
+    foodS--;
+    if(foodS < 0)
+        foodS = 0;
+    writeStock(foodS);
+    
+  }
+
+
+  drawSprites();
+  //add styles here
+  textSize(20);
+  fill("black");
+  text("Food remaining:" + foodS, 100,50);
+
+}
+
+function readStocks(data){
+  foodS=data.val();
+}
+
+function writeStock(x){
+  database.ref('/').update({
+    Food:x
+  })
+}
+
+
